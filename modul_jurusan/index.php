0d1be71622e40d27b3a3e4a8c63eeef331207752
Anggraini12 <pentianggrainii@gmail.com>
2024-11-13 15:18:05 +0700

    memperbaiki link navbar dan membuat index jurusan

--- modul_jurusan/index.php
+++ modul_jurusan/index.php
@@ -3,18 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projek IS-62</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Data Jurusan</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
 </head>
 <body>
-  <?php include_once('navbar.php');
+  <?php include_once('../navbar.php');
   ?>
 <div class="container">
     <div class="row mt-5">
         <div class ="col-6"></div>
         <div class="card">
   <div class="card-header text-center">
-    <h3>Sistem Informasi Mahasiswa</h3>
+    <h3>Data Jurusan</h3>
   </div>
   <div class="card-body ">
     <h4> Selamat Datang Administrator</h4>
@@ -24,7 +24,7 @@
   </div>
   </div>
 
-    <script src="js/bootstrap.js"></script>
-    <script src="js/bootstrap.bundle.js"></script>
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
 </body>
 </html>
